6b656ec28488c20e78fa7c9c6fe9b9ae3cce25b5
PRunich <p.runich@oblakogroup.ru>
2021-08-30 11:32:52 +0300

    added page for create link units

---
+++ src/routes/link_units/new.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	let form_data, url, note: string
+
+	$: form_data = {'link_unit': { 'url': url, 'note': note} }
+
+	async function formHandler(event: Event) {
+		event.preventDefault()
+
+		const response = await fetch('http://localhost:3000/link_units', { method: 'POST', headers: { 'Content-Type': 'application/json;charset=utf-8' }, body: JSON.stringify(form_data)})
+		alert(response.status)
+	}
+</script>
+
+<form on:submit={formHandler} name='link_unit'>
+	<input bind:value={url} type='text' placeholder='link' name='url'/>
+	<input bind:value={note} type='text' placeholder='note' name='note'/>
+	<input type='submit'>
+</form>
