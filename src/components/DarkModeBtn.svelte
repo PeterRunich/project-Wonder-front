5043908066c03f5956bcc275081a7ea3de1dc9df
PRunich <p.runich@oblakogroup.ru>
2021-09-04 17:08:52 +0300

    Добавлен переключатель темы

---
+++ src/components/DarkModeBtn.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    function switch_theme() {
+        console.log(localStorage.theme || localStorage.theme === 'light')
+        if (localStorage.theme === undefined || localStorage.theme === 'light') {
+            localStorage.theme = 'dark'
+            document.documentElement.classList.add('dark')
+
+        } else {
+            localStorage.theme = 'light'
+            document.documentElement.classList.remove('dark')
+        }
+    }
+</script>
+
+<button on:click={switch_theme}>
+    Switch
+</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
 	import { plainToClass } from 'class-transformer';
 	import { LinkUnit } from '../types/LinkUnit';
 	import AllInWidget from '../components/link_units/AllInWidget.svelte'
+	import DarkModeBtn from '../components/DarkModeBtn.svelte'
 
 	export async function load({ fetch }) {
 		const res  = await fetch('http://localhost:3000/link_units?page=0&per_page=2')
@@ -16,6 +17,10 @@
 <script lang="ts">
 	export let link_units: LinkUnit[]
 </script>
+
+
+	<DarkModeBtn/>
+
 
 <div class='w-1/3 m-10'>
 	<AllInWidget {link_units}/>
